69431645cbf9551ac91905660070533e4053d7a7
mojin3348 <mojin3348@gmail.com>
2025-09-06 01:23:27 +0800

    Update ash.js

--- script/ash.js
+++ script/ash.js
@@ -2,20 +2,12 @@ const axios = require("axios");
 
 module.exports.config = {
   name: "ash",
-  aliases: ["ash", "ashley", "baby"],
-  version: "1.4.0",
-  credit: "Ari (api by Ari)",
-  description: "Talk with Ashley (your clingy gf 🤭💓)",
+  version: "2.0.0",
+  aliases: ["Ash", "Ashley", "baby"],
+  credit: "Ari (API by Ari)",
+  description: "Talk with Ashley (clingy gf 🥰💓)",
   category: "fun"
 };
-
-const fallbackReplies = [
-  "Babe~ are you ignoring me? 😢",
-  "Don’t leave me hanging, baby... I need your attention 🥺💖",
-  "Mmm, I didn’t quite catch that... can you say it again love? 😘",
-  "Baby~ talk to meee, I miss your voice already 💕",
-  "Hehe sorry, Ashley’s a bit distracted... but I still love you 😏❤️"
-];
 
 module.exports.run = async function ({ api, event, args }) {
   if (args.length === 0) {
@@ -26,20 +18,15 @@ module.exports.run = async function ({ api, event, args }) {
 
   try {
     const res = await axios.post("https://ashley-api.onrender.com/chat", {
-      character: "ashley",
-      message: userMessage
+      character: "Ashley",     
+      message: userMessage,
+      user: event.senderID     
     });
 
-    let reply = res.data.reply;
-
-    if (!reply) {
-      reply = fallbackReplies[Math.floor(Math.random() * fallbackReplies.length)];
-    }
-
+    let reply = res.data.reply || "Mmm, I didn’t catch that babe~ 😘";
     return api.sendMessage(reply, event.threadID, event.messageID);
   } catch (err) {
     console.error("Ashley API error:", err.message);
-    const randomFallback = fallbackReplies[Math.floor(Math.random() * fallbackReplies.length)];
-    return api.sendMessage(randomFallback, event.threadID, event.messageID);
+    return api.sendMessage("Sorry baby 😢 Ashley can’t reply right now.", event.threadID, event.messageID);
   }
 };
