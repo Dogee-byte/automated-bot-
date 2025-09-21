af9f81a0cd72e9b5b1a077e2ac5c6d1e3d295921
mojin3348 <mojin3348@gmail.com>
2025-09-21 18:18:24 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,18 @@
+const backgroundm = document.querySelector(".play-background");
+    Swal.fire({
+      title: `<p style="font-size: 15px; margin-top: -15px; margin-bottom: -10px;">Ryuko V5</p>`,
+      icon: "info",
+      html: `<p style="font-size: 8px;">This project is not for sale, you can use it for free.</p>`,
+      background: '#1e1e1e',
+      color: 'white',
+      confirmButtonColor: 'transparent',
+      confirmButtonText: `
+    <i class="fa fa-thumbs-up" style="color:white;"></i> Okay
+  `
+    }).then(() => {
+        backgroundm.play();
+    });
+
 document.getElementById('agreeCheckbox').addEventListener('change', function() {
   document.getElementById('submitButton').disabled = !this.checked;
 });
